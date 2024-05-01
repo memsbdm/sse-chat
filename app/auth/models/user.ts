bdce822471733cd962c8038101d47f334fe10b4d
Memsbdm <mehmetbdm@outlook.fr>
2024-05-01 14:37:21 +0200

    refactor(auth): user model and migration

--- app/auth/models/user.ts
+++ app/auth/models/user.ts
@@ -3,28 +3,31 @@ import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { Opaque } from '@adonisjs/core/types/helpers'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
   passwordColumnName: 'password',
 })
 
+export type UserId = Opaque<'UserId', string>
+
 export default class User extends compose(BaseModel, AuthFinder) {
   @column({ isPrimary: true })
-  declare id: number
-
-  @column()
-  declare fullName: string | null
-
-  @column()
-  declare email: string
-
-  @column()
-  declare password: string
+  declare id: UserId
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @column()
+  declare username: string | null
+
+  @column()
+  declare email: string
+
+  @column()
+  declare password: string
 }
--- database/migrations/1714557092364_create_users_table.ts
+++ database/migrations/1714557092364_create_users_table.ts
@@ -5,13 +5,12 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
-      table.string('full_name').nullable()
-      table.string('email', 254).notNullable().unique()
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('gen_random_uuid()').knexQuery)
+      table.timestamp('created_at', { useTz: false }).notNullable()
+      table.timestamp('updated_at', { useTz: false }).notNullable()
+      table.string('username').notNullable().unique()
+      table.string('email').notNullable().unique()
       table.string('password').notNullable()
-
-      table.timestamp('created_at').notNullable()
-      table.timestamp('updated_at').nullable()
     })
   }
 
