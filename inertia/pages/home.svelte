ba0c88ef1d40f38688dfa57494d04f3894ec5417
Memsbdm <mehmetbdm@outlook.fr>
2024-05-01 15:59:17 +0200

    feat(auth): add logout

--- inertia/pages/home.svelte
+++ inertia/pages/home.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-export let version: number
+  // @ts-ignore
+  import { router } from '@inertiajs/svelte'
+  export let version: number
+
+  function handleSubmit(){
+    router.visit('/auth/logout', { method: 'delete' })
+  }
 </script>
 
 <svelte:head>
@@ -13,4 +19,19 @@ export let version: number
     Learn more about AdonisJS and Inertia.js by visiting the
     <a href="https://docs.adonisjs.com/guides/inertia">AdonisJS documentation</a>.
   </span>
+  <form on:submit|preventDefault={handleSubmit}>
+    <button type="submit">Logout</button>
+  </form>
 </div>
+
+<style>
+  button{
+    margin-top: 1rem;
+    padding: .5rem 2rem;
+    background-color: #ff0000;
+    color: white;
+    border: none;
+    outline: none;
+    cursor: pointer;
+  }
+</style>
